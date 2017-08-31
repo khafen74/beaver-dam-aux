a56256d32006b50453ee5355d393e8a3452dae5d
KONRADSPC\khafe <khafen74@gmail.com>
2017-08-31 08:33:12 -0700

    streamlining of merge script

--- raster_merge/raster_merge.py
+++ raster_merge/raster_merge.py
@@ -2,6 +2,7 @@ import os
 from osgeo import gdal
 
 import subprocess
+
 filea = r'F:\01_etal\Projects\Modeling\BeaverWaterStorage\wrk_Data\AnalysisRuns\BearRiverHUC8\16010101\02_rasIn\dem_vb.tif'
 fileb = r'F:\01_etal\Projects\Modeling\BeaverWaterStorage\wrk_Data\AnalysisRuns\BearRiverHUC8\16010102\02_rasIn\dem_vb.tif'
 filec = r'F:\01_etal\Projects\Modeling\BeaverWaterStorage\wrk_Data\AnalysisRuns\BearRiverHUC8\16010201\02_rasIn\dem_vb.tif'
@@ -51,10 +52,62 @@ class Merger():
 
 #code to run for merging data from all HUC12s
 
-basePath = '03_out_100'
-basePath = 'MODFLOW_100'
-hucName = '16010203'
-outDir = "03_out_100_merge"
+# basePath = '03_out_100'
+# #basePath = 'MODFLOW_100'
+# hucName = '16010204'
+# outDir = "03_out_100_merge"
+# mainDir = "F:/01_etal/Projects/Modeling/BeaverWaterStorage/wrk_Data/AnalysisRuns/BearRiverHUC8"
+#
+# dataFile1 = basePath + '/depMid.tif'
+# dataFile2 = basePath + '/depLo.tif'
+# dataFile3 = basePath + '/depHi.tif'
+#
+# dataFile4 = basePath + '/hdch_mid_fc.tif'
+# dataFile5 = basePath + '/hdch_lo_fc.tif'
+# dataFile6 = basePath + '/hdch_hi_fc.tif'
+#
+# dataFile1 = basePath + '/pondID.tif'
+#
+# copyDir = mainDir + '/' + hucName + '/HUC12'
+#
+# outPath1 = mainDir + '/' + hucName + '/' + outDir + '/depMid_m.tif'
+# outPath2 = mainDir + '/' + hucName + '/' + outDir + '/depLo_m.tif'
+# outPath3 = mainDir + '/' + hucName + '/' + outDir + '/depHi_m.tif'
+#
+# outPath4 = mainDir + '/' + hucName + '/' + outDir + '/hdch_mid_m3.tif'
+# outPath5 = mainDir + '/' + hucName + '/' + outDir + '/hdch_lo_m3.tif'
+# outPath6 = mainDir + '/' + hucName + '/' + outDir + '/hdch_hi_m3.tif'
+#
+# outPath1 = mainDir + '/' + hucName + '/' + outDir + '/pondID.tif'
+#
+# # dataFile = [dataFile1, dataFile2, dataFile3]
+# # outPath = [outPath1, outPath2, outPath3]
+#
+# #dataFile = [dataFile4, dataFile5, dataFile6]
+# #outPath = [outPath4, outPath5, outPath6]
+#
+# #for multiple files
+# dataFile = [dataFile1]
+# outPath = [outPath1]
+#
+# for i in range(0, len(dataFile), 1):
+#     tester = Merger(dataFile[i], copyDir, outPath[i])
+#     tester.run()
+#
+# #for single file
+# # tester = Merger(dataFile1, copyDir, outPath1)
+# # tester.run()
+
+##########################################################################################
+############# Code to run to merge HUC12s for each HUC8 ##################################
+##########################################################################################
+
+cap = '05'
+basePath = '03_out_' + cap
+print basePath
+# basePath = 'MODFLOW_100'
+hucNames = ['16010101', '16010102', '16010201', '16010202', '16010203', '16010204']
+outDir = basePath +"_merge"
 mainDir = "F:/01_etal/Projects/Modeling/BeaverWaterStorage/wrk_Data/AnalysisRuns/BearRiverHUC8"
 
 dataFile1 = basePath + '/depMid.tif'
@@ -65,55 +118,74 @@ dataFile4 = basePath + '/hdch_mid_fc.tif'
 dataFile5 = basePath + '/hdch_lo_fc.tif'
 dataFile6 = basePath + '/hdch_hi_fc.tif'
 
+dataFile1 = basePath + '/pondID.tif'
 
-copyDir = mainDir + '/' + hucName + '/HUC12'
+for hucName in hucNames:
+    copyDir = mainDir + '/' + hucName + '/HUC12'
 
-outPath1 = mainDir + '/' + hucName + '/' + outDir + '/depMid_m.tif'
-outPath2 = mainDir + '/' + hucName + '/' + outDir + '/depLo_m.tif'
-outPath3 = mainDir + '/' + hucName + '/' + outDir + '/depHi_m.tif'
+    outPath1 = mainDir + '/' + hucName + '/' + outDir + '/depMid_m.tif'
+    outPath2 = mainDir + '/' + hucName + '/' + outDir + '/depLo_m.tif'
+    outPath3 = mainDir + '/' + hucName + '/' + outDir + '/depHi_m.tif'
 
-outPath4 = mainDir + '/' + hucName + '/' + outDir + '/hdch_mid_m3.tif'
-outPath5 = mainDir + '/' + hucName + '/' + outDir + '/hdch_lo_m3.tif'
-outPath6 = mainDir + '/' + hucName + '/' + outDir + '/hdch_hi_m3.tif'
+    outPath4 = mainDir + '/' + hucName + '/' + outDir + '/hdch_mid_m3.tif'
+    outPath5 = mainDir + '/' + hucName + '/' + outDir + '/hdch_lo_m3.tif'
+    outPath6 = mainDir + '/' + hucName + '/' + outDir + '/hdch_hi_m3.tif'
 
-# dataFile = [dataFile1, dataFile2, dataFile3]
-# outPath = [outPath1, outPath2, outPath3]
+    outPath1 = mainDir + '/' + hucName + '/' + outDir + '/pondID.tif'
 
-dataFile = [dataFile4, dataFile5, dataFile6]
-outPath = [outPath4, outPath5, outPath6]
+    # dataFile = [dataFile1, dataFile2, dataFile3]
+    # outPath = [outPath1, outPath2, outPath3]
 
-for i in range(0, len(dataFile), 1):
-    tester = Merger(dataFile[i], copyDir, outPath[i])
-    tester.run()
+    # dataFile = [dataFile4, dataFile5, dataFile6]
+    # outPath = [outPath4, outPath5, outPath6]
+
+    dataFile = [dataFile1]
+    outPath = [outPath1]
+
+    for i in range(0, len(dataFile), 1):
+        tester = Merger(dataFile[i], copyDir, outPath[i])
+        tester.run()
 
 ##########################################################################################
 ############# Code to run to merge HUC8s for entire basin ################################
 ##########################################################################################
 
-# basePath = '03_out_100_merge'
-# subDir = 'EntireBasin'
-# outDir = "out_100"
-# mainDir = "F:/01_etal/Projects/Modeling/BeaverWaterStorage/wrk_Data/AnalysisRuns/BearRiverHUC8"
-#
-# dataFile1 = basePath + '/depMid_m.tif'
-# dataFile2 = basePath + '/depLo_m.tif'
-# dataFile3 = basePath + '/depHi_m.tif'
-# dataFile4 = basePath + '/hdch_mid_m3.tif'
-# dataFile5 = basePath + '/hdch_lo_m3.tif'
-# dataFile6 = basePath + '/hdch_hi_m3.tif'
-#
-# copyDir = mainDir
-#
-# outPath1 = mainDir + '/' + subDir + '/' + outDir + '/depMid_m.tif'
-# outPath2 = mainDir + '/' + subDir + '/' + outDir + '/depLo_m.tif'
-# outPath3 = mainDir + '/' + subDir + '/' + outDir + '/depHi_m.tif'
-# outPath4 = mainDir + '/' + subDir + '/' + outDir + '/hdch_mid_m3.tif'
-# outPath5 = mainDir + '/' + subDir + '/' + outDir + '/hdch_lo_m3.tif'
-# outPath6 = mainDir + '/' + subDir + '/' + outDir + '/hdch_hi_m3.tif'
-#
+#old method
+#basePath = '03_out_50_merge'
+
+print 'merging huc8s for entire region'
+
+#new method, base path already set by huc12
+basePath = outDir
+subDir = 'EntireBasin'
+outDir = "out_" + cap
+mainDir = "F:/01_etal/Projects/Modeling/BeaverWaterStorage/wrk_Data/AnalysisRuns/BearRiverHUC8"
+
+dataFile1 = basePath + '/depMid_m.tif'
+dataFile2 = basePath + '/depLo_m.tif'
+dataFile3 = basePath + '/depHi_m.tif'
+dataFile4 = basePath + '/hdch_mid_m3.tif'
+dataFile5 = basePath + '/hdch_lo_m3.tif'
+dataFile6 = basePath + '/hdch_hi_m3.tif'
+
+dataFile1 = basePath + '/pondID.tif'
+
+copyDir = mainDir
+
+outPath1 = mainDir + '/' + subDir + '/' + outDir + '/depMid_m.tif'
+outPath2 = mainDir + '/' + subDir + '/' + outDir + '/depLo_m.tif'
+outPath3 = mainDir + '/' + subDir + '/' + outDir + '/depHi_m.tif'
+outPath4 = mainDir + '/' + subDir + '/' + outDir + '/hdch_mid_m3.tif'
+outPath5 = mainDir + '/' + subDir + '/' + outDir + '/hdch_lo_m3.tif'
+outPath6 = mainDir + '/' + subDir + '/' + outDir + '/hdch_hi_m3.tif'
+
+outPath1 = mainDir + '/' + subDir + '/' + outDir + '/pondID.tif'
+
 # dataFile = [dataFile1, dataFile2, dataFile3, dataFile4, dataFile5, dataFile6]
 # outPath = [outPath1, outPath2, outPath3, outPath4, outPath5, outPath6]
-#
-# for i in range(0, len(dataFile), 1):
-#     tester = Merger(dataFile[i], copyDir, outPath[i])
-#     tester.run()
+dataFile = [dataFile1]
+outPath = [outPath1]
+
+for i in range(0, len(dataFile), 1):
+    tester = Merger(dataFile[i], copyDir, outPath[i])
+    tester.run()
